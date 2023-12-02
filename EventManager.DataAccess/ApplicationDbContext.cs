26097f759756b3f8f8c5d41c0eacd1116b2eae47
Szymon Fiedorowicz <szymon.fiedorowicz@gmail.com>
2023-12-02 12:47:20 +0100

    Add project files.

---
+++ EventManager.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EventManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManager.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ EventManager.Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Models
+{
+    public class Event
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Capacity { get; set; }
+
+        /// ForeignKey
+        //public int Creator_Id { get; set; }
+    }
+}
