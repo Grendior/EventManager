3a3900da5a3ceb927d45b86ae207f9c24aa6582f
Szymon Fiedorowicz <szymon.fiedorowicz@gmail.com>
2023-12-17 14:12:33 +0100

    Wstępne dodanie wysyłanie maili

--- EventManager.Utils/EmailSender.cs
+++ EventManager.Utils/EmailSender.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using PostmarkDotNet;
+using PostmarkDotNet.Model;
 
 namespace EventManager.Utils
 {
@@ -6,8 +8,17 @@ namespace EventManager.Utils
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            // logic to send email
-            return Task.CompletedTask;
+            var message = new PostmarkMessage()
+            {
+                To = email,
+                From = "szymon.fiedorowicz@witkac.pl",
+                Subject = subject,
+                HtmlBody = htmlMessage,
+            };
+
+            var client = new PostmarkClient("84b63a08-aa29-413c-8a01-ae6a01dee7cf");
+
+            return client.SendMessageAsync(message);
         }
     }
 }
